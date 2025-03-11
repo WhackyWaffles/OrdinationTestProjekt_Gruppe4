dc3725b3d8ac8cf2e63e122b09dbd631336dfc5a
mmaegaard <eaa24khmm@students.eaaa.dk>
2025-03-11 12:53:14 +0100

    Har lavet en DagligSkaev

--- src/ordination/DagligSkaev.java
+++ src/ordination/DagligSkaev.java
@@ -1,27 +1,43 @@
 package ordination;
 
+import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.ArrayList;
 
 public class DagligSkaev extends Ordination {
     // TODO
+    private ArrayList<Dosis> doser = new ArrayList<>();
 
+    public DagligSkaev(LocalDate startDato, LocalDate slutDato) {
+        super(startDato, slutDato);
+    }
+
+    public ArrayList<Dosis> getDoser() {
+        return doser;
+    }
 
     public void opretDosis(LocalTime tid, double antal) {
         // TODO
+        doser.add(new Dosis(tid, antal));
     }
 
     @Override
     public double samletDosis() {
-        return 0;
+        return doegnDosis() * (double) super.antalDage();
     }
 
     @Override
     public double doegnDosis() {
-        return 0;
+            double sumDosis = 0;
+            for (Dosis dskaev : doser) {
+                sumDosis += dskaev.getAntal();
+            }
+        return sumDosis;
     }
+
 
     @Override
     public String getType() {
-        return "";
+        return "DagligSkaev";
     }
 }
